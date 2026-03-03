0a91a74e3328f4aac7ef29d5285a738f4d66b7e4
CsCppKotlin <CsCppKotlin@gmail.com>
2026-03-03 09:25:44 +0300

    WPF ViewModels

---
+++ BatterySerialMonitor/ViewModels/ConnectionViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using System.IO.Ports;
+using BatteryTracker.Core.Models;
+
+namespace BatterySerialMonitor.ViewModels
+{
+    public class ConnectionViewModel : ViewModelBase
+    {
+        // Сущность из твоей библиотеки Core
+        public SerialSettings Settings { get; } = new SerialSettings();
+
+        public ObservableCollection<string> AvailablePorts { get; } = new();
+        public ObservableCollection<int> BaudRates { get; } = new() { 9600, 19200, 38400, 57600, 115200 };
+
+        public ConnectionViewModel()
+        {
+            RefreshPorts();
+        }
+
+        public string SelectedPort
+        {
+            get => Settings.PortName;
+            set { Settings.PortName = value; OnPropertyChanged(); }
+        }
+
+        public int SelectedBaudRate
+        {
+            get => Settings.BaudRate;
+            set { Settings.BaudRate = value; OnPropertyChanged(); }
+        }
+
+        public void RefreshPorts()
+        {
+            AvailablePorts.Clear();
+            foreach (var port in SerialPort.GetPortNames()) AvailablePorts.Add(port);
+        }
+    }
+}
